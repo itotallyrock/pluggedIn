36e3a9dc34822cd84a43464401fc4ce7be8d4666
itotallyrock <itotallyrock@users.noreply.github.com>
2014-11-26 14:31:55 -0600

    more logging

--- js/pluggedIn.js
+++ js/pluggedIn.js
@@ -345,6 +345,7 @@ pluggedIn = {
 			
 			if(pluggedIn.settings.notifications.songStats){
 				API.on(API.HISTORY_UPDATE, function(e){
+					pluggedIn.core.info(JSON.stringify(e[0]),true);
 					pluggedIn.core.log(e[0].media.title+'\n'+e[0].score.positive+' Woots\n'+e[0].score.grabs+' Grabs\n'+e[0].score.negative+' Mehs');
 					$("#chat-messages").append('<div style="color: #d800cd;" class="message"><span class="text" style="font-weight:300;"><strong>'+e[0].media.title+'</strong><br/>'+e[0].score.positive+' Woots<br/>'+e[0].score.grabs+' Grabs<br/>'+e[0].score.negative+' Mehs</span></div>');
 					$('#chat-messages').scrollTop($('#chat-messages')[0].scrollHeight);
